c5fdcdf15d122d1bf30055910be6dbcb7ecc33b7
Maxim Reznik <reznikmm@gmail.com>
2017-01-30 11:11:07 +0200

    Move servlet binding from source to web.xml

--- src/startup/startup.adb
+++ src/startup/startup.adb
@@ -1,5 +1,5 @@
 ------------------------------------------------------------------------------
---  Copyright © 2016, Maxim Reznik <reznikmm@gmail.com>
+--  Copyright © 2016-2017, Maxim Reznik <reznikmm@gmail.com>
 --  All rights reserved.
 --
 --  Redistribution and use in source and binary forms, with or without
@@ -28,23 +28,13 @@
 
 with Ada.Text_IO;
 
-with League.Strings;
-with League.String_Vectors;
-
-with Servlet.Servlet_Registrations;
-with Servlet.Servlets;
-
 with AWFC.Static_Resource_Servlets;
+pragma Unreferenced (AWFC.Static_Resource_Servlets);
 
 with Axe.Wiki_View_Servlets;
+pragma Unreferenced (Axe.Wiki_View_Servlets);
 
 package body Startup is
-
-   type Servlet_Access is access all Servlet.Servlets.Servlet'Class;
-
-   function "+"
-    (Item : Wide_Wide_String) return League.Strings.Universal_String
-       renames League.Strings.To_Universal_String;
 
    ----------------
    -- On_Startup --
@@ -54,52 +44,9 @@ package body Startup is
      (Self    : in out Servlet_Container_Initializer;
       Context : in out Servlet.Contexts.Servlet_Context'Class)
    is
-      pragma Unreferenced (Self);
-
-      Registry : access
-        Standard.Servlet.Servlet_Registrations.Servlet_Registration'Class;
-
-      List : League.String_Vectors.Universal_String_Vector;
+      pragma Unreferenced (Self, Context);
    begin
       Ada.Text_IO.Put_Line ("I'm here!");
-
-      --  Create and register servlets.
-
-      Registry := Context.Add_Servlet
-        (+"StaticResources",
-         Servlet_Access'(new AWFC.Static_Resource_Servlets
-                                   .Static_Resource_Servlet));
-      List.Append (+"/acats/*");
-      List.Append (+"/arm83/*");
-      List.Append (+"/asis_ugrg_RU/*");
-      List.Append (+"/files/*");
-      List.Append (+"/glade_UG_RU/*");
-      List.Append (+"/graphics/*");
-      List.Append (+"/GtkAda/*");
-      List.Append (+"/i432/*");
-      List.Append (+"/icons/*");
-      List.Append (+"/Lovelace/*");
-      List.Append (+"/V-0.4w/*");
-      List.Append (+"*.exe");
-      List.Append (+"*.wiki");
-      List.Append (+"*.css");
-      List.Append (+"*.xml");
-      List.Append (+"*.png");
-      List.Append (+"*.jpg");
-      List.Append (+"*.txt");
-
-      List := Registry.Add_Mapping (List);
-
-      if not List.Is_Empty then
-         raise Program_Error;
-      end if;
-
-      Registry := Context.Add_Servlet
-        (+"WikiRendering",
-         Servlet_Access'(new Axe.Wiki_View_Servlets.Wiki_View_Servlet));
-      Registry.Add_Mapping (+"/");
-      Registry.Add_Mapping (+"/edit_wiki/*");
-
       --  TODO: /arm/*
       --  TODO: set_password.html
    end On_Startup;
